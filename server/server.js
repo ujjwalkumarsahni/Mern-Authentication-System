a25e141bbed5da566d0c2b55f1e1736b04911554
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2025-02-17 22:26:47 +0530

    node mailer

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import cookieParser from 'cookie-parser'
-
-// importing data base
 import ConnectDB from './config/mongoDB.js'
+import authRouter from './routes/authRoutes.js'
+
+
 const app = express()
 const port = process.env.PORT || 4000;
 
@@ -15,9 +16,10 @@ app.use(express.json())
 app.use(cors({credentials: true}))
 app.use(cookieParser())
 
-app.get('/', (req,res) =>{
-    res.send("Api working fine");
-})
+
+// api endpoint
+app.get('/', (req,res) => res.send("Api working fine"))
+app.use('/api/auth',authRouter)
 
 app.listen(port, ()=>{
     console.log("Server runing on port" , port)
