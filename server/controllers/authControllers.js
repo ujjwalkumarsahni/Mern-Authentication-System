29ce71f240c8172375aee57ac206a10cba6b51bb
ujjwalkumarsahni <ujjwalkumar0514@gmail.com>
2025-03-14 19:08:03 +0530

    change and fix

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -35,12 +35,19 @@ export const register = async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
         // Setting the token in the cookie
-        res.cookie('token', token, {
+        // res.cookie('token', token, {
+        //     httpOnly: true,
+        //     secure: process.env.NODE_ENV === 'production',
+        //     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        //     maxAge: 7 * 24 * 60 * 60 * 1000,
+        // });
+
+        res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-        });
+            secure: true,  // Always true for production (HTTPS)
+            sameSite: "none", // Required for cross-origin cookies
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+          });
 
         // Sending welcome email
         const mailOptions = {
